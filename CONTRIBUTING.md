2a1798d0a910d363563918e7429a12ed6f148b6c
Trevor D. Miller <trevordmiller@icloud.com>
2017-05-27 22:18:14 +0000

    Add text message sending

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,6 +1,13 @@
 # Contributing
 
 - Ensure you have [Git](https://git-scm.com/) and [Node](https://nodejs.org) installed on your machine
+- Add a `.env` file with sensitive environment variables like:
+  ```
+  ACCOUNT_SID='abc'
+  AUTH_TOKEN='abc'
+  FROM_PHONE_NUMBER='+12223334444'
+  TO_PHONE_NUMBER='+12223334444'
+  ```
 - Run `npm install` to install libraries
 - Run `npm run dev` to develop
 - Submit a pull request to `master`
--- index.js
+++ index.js
@@ -1,15 +1,34 @@
+require('dotenv').config()
 const Gpio = require('onoff').Gpio
+const twilio = require('twilio')
+const debounce = require('debounce')
+
+const {
+  ACCOUNT_SID, 
+  AUTH_TOKEN, 
+  FROM_PHONE_NUMBER, 
+  TO_PHONE_NUMBER,
+} = process.env
 
 const button = new Gpio(3, 'in', 'falling')
-console.log(button) 
+
+const phone = new twilio(ACCOUNT_SID, AUTH_TOKEN)
+
+const sendMessage = debounce(() => {
+  phone.messages.create({
+    from: FROM_PHONE_NUMBER,
+    to: TO_PHONE_NUMBER,
+    body: 'I need to go to the bathroom!', 
+  }) 
+}, 10000, true)
 
 button.watch((err) => {
   if (err) {
     throw err
   }
-  console.log('hit')
+  sendMessage()
 })
 
 process.on('SIGINT', () => {
-  button.unexport()
+  button.unexport() 
 })
